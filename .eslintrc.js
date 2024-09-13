e299b236fd124b5ab2f9affc574afc2e4fa3b25c
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-09-13 19:58:23 +0900

    Init:ESLint와 Prettier 설치

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier"
+    ],
+    plugins: [
+        "prettier"
+    ],
+    rules: {
+        "prettier/prettier": "error"
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        browser: true,
+        node: true
+    }
+};
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier"
+    ],
+    plugins: [
+        "prettier"
+    ],
+    rules: {
+        "prettier/prettier": "error"
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        browser: true,
+        node: true
+    }
+};
