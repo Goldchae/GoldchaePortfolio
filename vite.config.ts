34e98b3e96e238e657992b4e8eb1ca623ca4af64
JunEunChae <celina03240@ewhain.net>
2024-09-14 20:30:04 +0900

    init : Typescript, React, Vite, Emotion TailwindCSS, Framer Motion, Three.js,ESLint,Prettier,Jest,등 설치와 와 vite.config.ts/tsconfig.json/eslint.config.js/.prettierrc 및 기타 파일 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
