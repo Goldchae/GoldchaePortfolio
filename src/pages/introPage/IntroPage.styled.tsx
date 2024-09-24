281636b9887966cefc52e87c753038258ed8054e
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-09-24 20:33:03 +0900

    fix:통합 수정

--- src/pages/introPage/IntroPage.styled.tsx
+++ src/pages/introPage/IntroPage.styled.tsx
@@ -2,6 +2,6 @@ import styled from '@emotion/styled';
 
 export const IntroPageContainer = styled.div`
     position: absolute;
-    width: 100%;
-    //aspect-ratio: 6 / 5;
-`;
+    width: 100vw; 
+    aspect-ratio: 6 / 3.5;
+`
