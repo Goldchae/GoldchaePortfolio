6a9d580e85bd77116cb2c57a4fe2fe13f6b06cbe
JunEunChae <celina03240@ewhain.net>
2024-10-03 21:28:24 +0900

    feat : 메타 태그 추가

--- src/components/introPageComponents/introText/IntroText.tsx
+++ src/components/introPageComponents/introText/IntroText.tsx
@@ -9,7 +9,7 @@ const IntroText: React.FC = () => {
             <StarFont
               value="HI THERE!"
               fontFamily="StarBold"
-              fontSize = "3.9vw"
+              fontSize = "3.5vw"
               color="#ffffff"
             />
           </TitleTextContainer>
@@ -18,7 +18,7 @@ const IntroText: React.FC = () => {
         <StarFont
           value="현재 이화여자대학교 컴퓨터공학과 22학번, 3학년 휴학 중인 정은채입니다!"
           fontFamily="StarLight"
-          fontSize = "1.6vw"
+          fontSize = "1.3vw"
           color="#ffffff"
         />
       </BoduTextContainer>
@@ -26,7 +26,7 @@ const IntroText: React.FC = () => {
         <StarFont
           value="협업과 화합을 통한 성장을 중요하게 여겨 지속적으로 다양한 단체에서 리더를 맡으며 다양한 사람들과 시너지를 내고 있습니다."
           fontFamily="StarLight"
-          fontSize = "1.6vw"
+          fontSize = "1.3vw"
           color="#ffffff"
         />
       </Bodu2TextContainer>
@@ -34,7 +34,7 @@ const IntroText: React.FC = () => {
         <StarFont
           value="Frontend, Backend, AI, DevOps 등에 관심을 가지고 있으며 T자형 인재가 되고자 노력합니다."
           fontFamily="StarLight"
-          fontSize = "1.6vw"
+          fontSize = "1.3vw"
           color="#ffffff"
         />
       </Bodu3TextContainer>
